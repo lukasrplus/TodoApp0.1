e7c7bdfa8972f81d2c82256a396fe9afe059a284
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-10-27 17:17:18 -0500

    Added Task Counter (also for important tasks)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,18 @@ class TasksController < ApplicationController
 
 
   def index
+
     @tasks = Task.all
+
+    important_tasks = Array.new
+    @tasks.each do |t|
+      if t.important
+        important_tasks << t
+      end
+    end
+
+    @count_important_tasks = important_tasks.count
+
   end
 
   def new
@@ -12,7 +23,6 @@ class TasksController < ApplicationController
    t = Task.new
    t.content = params["content"]
    t.owner = params["owner"]
-   t.done = params["done"]
    t.important = params["important"]
    t.save
    redirect_to root_url
@@ -26,22 +36,12 @@ end
 
 def edit
 @task = Task.find_by(id: params[:id])
-
-  if @task.done == "true"
-    @taskdone = "on"
-  end
-
-  if @task.important == "true"
-    @taskimportant = "on"
-  end
-
 end
 
 def update
 task = Task.find_by(id: params[:id])
 task.content = params["content"]
 task.owner = params["owner"]
-task.done = params["done"]
 task.important = params["important"]
 task.save
 redirect_to root_url
--- db/migrate/20131026185213_create_tasks.rb
+++ db/migrate/20131026185213_create_tasks.rb
@@ -3,7 +3,6 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.string :content
       t.string :owner
-      t.boolean :done
       t.boolean :important
     end
   end
