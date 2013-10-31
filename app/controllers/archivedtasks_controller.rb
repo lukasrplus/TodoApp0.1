46789cbaf458eef2a9564d5988140aed7bc8374a
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-10-31 11:15:57 -0500

    Added UNDO from archive

--- app/controllers/archivedtasks_controller.rb
+++ app/controllers/archivedtasks_controller.rb
@@ -4,6 +4,25 @@ def index
   @archivedtasks = Archivedtask.all
 end
 
+def undo
+
+  archivedtask = Archivedtask.find_by(id: params[:id])
+
+  new_task = Task.new
+
+  new_task.content = archivedtask.content
+  new_task.owner = archivedtask.owner
+  new_task.important = archivedtask.important
+
+  new_task.save
+  archivedtask.destroy
+  redirect_to archivedtasks_url
+
+end
+
+def destroy
+end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TodoApp01::Application.routes.draw do
 
+
+# TASKSCONTROLLER
+
 get '/tasks' => 'tasks#index'
 
 get '/tasks/new' => 'tasks#new', :as => "new_task"
@@ -12,17 +15,14 @@ delete '/tasks/:id' => 'tasks#destroy'
 get '/tasks/:id/edit' => 'tasks#edit', :as => "edit_task"
 patch '/tasks/:id' => 'tasks#update'
 
-
-
-# CREATE A NEW CONTROLLER/RESSOURCE FOR ARCHIVE
-
-
-
-
-
 get '/tasks/:id/archive' => 'tasks#archive', :as => "archive_task"
 
 
+# ARCHIVEDTASKS CONTROLLER
+
 get '/archivedtasks' => 'archivedtasks#index'
+
+get '/archivedtasks/:id/undo' => 'archivedtasks#undo', :as => "undo_archivedtask"
+delete '/archivedtasks/:id' => 'archivedtasks#destroy'
 
 end
