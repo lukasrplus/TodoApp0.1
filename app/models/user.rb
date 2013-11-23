88c3911cc4de1d435e55d463ebc6b2f6fd31945a
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-11-23 17:28:35 -0600

    Simple JS for Signup Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ has_secure_password
 validates_presence_of :username, :on => :create
 validates_presence_of :email, :on => :create
 
-validates_presence_of :password, :on => :create
-validates_presence_of :password_confirmation, :on => :create
+# validates_presence_of :password, :on => :create
+# validates_presence_of :password_confirmation, :on => :create
 
 has_many :tasks
 
