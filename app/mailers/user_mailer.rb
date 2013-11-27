e1b9da2ca34d6f2749507969c059d2b15695b85b
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2013-11-27 12:32:31 -0600

    Working Signup Email with picture but without Font Awesome

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "lukasrplus@yahoo.com"
+  default from: "tasksmashr@gmail.com"
 
   def send_signup_welcome(user)
     @u = user
     # gets the user from the CREATE Controller
 
-    mail to: user.email, :subject => "Congrats. You have become an officle TASKSMASHR"
+    mail to: user.email, :subject => "WELCOME TO TASKSMASHR"
 
   end
 
