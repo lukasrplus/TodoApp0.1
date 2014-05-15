0c575e67aa822a8f5e7ebf3cd0493926f4d790d0
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-05-15 14:54:50 +0200

    Some minor changes and ENV vars

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,20 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 TodoApp01::Application.initialize!
 
-
+if Rails.env.development?
 CREDENTIALS = YAML.load_file(Rails.root.join('config', 'email.yml'))
+end
+
+TASK_EMAIL = Rails.env.development? ? CREDENTIALS['username'] : ENV['TASK_EMAIL']
+TASK_PASSWORD = Rails.env.development? ? CREDENTIALS['password'] : ENV['TASK_PASSWORD']
+
+
+
+
 
 ActionMailer::Base.smtp_settings = {
-  user_name: CREDENTIALS['username'],
-  password: CREDENTIALS['password'],
+  user_name: TASK_EMAIL,
+  password: TASK_PASSWORD,
   address: 'smtp.gmail.com',
   port: 587,
   authentication: 'plain',
